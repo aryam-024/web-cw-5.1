c78c032b213dd37ce4fe1b7a86b0ece63181941a
aryam-024 <66679403+aryam-024@users.noreply.github.com>
2020-06-27 11:16:46 -0700

    تم الحل

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,34 +1,45 @@
 // السؤال الأول
 // 1
+const NAME ="aryam"; 
+
 
 // 2
-
+const number = 17;
 // 3 and 4
+console.log("اسمي"+name+" "+"عمري"+" "+number);
 
 // 5 and 6
+console.log( number*5);
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let anime =["haikyuu","kny","boku no hero"];
 // 2
-
+anime.push(17)
+console.log(anime)
 // 3
-
+anime.pop()
+console.log(anime)
 // 4
+let object1={
+path:"web",
+  language:"css",
+     
+};
 
-// 5
-
+// 5 
+object1.ta ="rahaf"
 // 6
-
+console.log(object1.path)
 // 7
-
-// -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
+console.log(object1.ta )
+// -.-.-.-.-.-.-.-.-.-.-.-..-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
 // 1
 
-// 2
+// 2 
 
 // console results => console.png
